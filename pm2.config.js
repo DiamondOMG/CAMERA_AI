784f8dff9d44c93059441e57fc078a039067df67
Mond Naja <omgdigital.developer.02@gmail.com>
2025-11-20 17:07:37 +0700

    Add PM2 configuration for FastAPI upload server with pythonw.exe

---
+++ pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fastapi-upload-server',
+      // *** เปลี่ยน python.exe เป็น pythonw.exe ***
+      script: 'C:/Users/natna/Desktop/OMG/CAMERA_AI/venv/Scripts/pythonw.exe', 
+
+      args: '-m uvicorn main:app --host 0.0.0.0 --port 8000',
+
+      watch: false,
+      exec_mode: 'fork',
+      instances: 1,
+    },
+  ],
+};
